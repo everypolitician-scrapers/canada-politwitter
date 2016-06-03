2829983364db1774bc979bd29274a2f6dc52b7c8
Tony Bowden <tony@tmtm.com>
2016-06-03 08:12:03 +0100

    Also get Facebook and YouTube

--- scraper.rb
+++ scraper.rb
@@ -18,19 +18,67 @@ def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
-def scrape_list(url)
+def twitter_list(url)
   noko = noko_for(url)
   table = noko.xpath('//table[.//th[.="username"]]')
-  table.xpath('.//tr[td]').each do |tr|
+  table.xpath('.//tr[td]').map do |tr|
     td = tr.css('td')
     data = {
+      id: td[0].css('a/@href').text.split('/').last,
       name: td[1].text.tidy,
       twitter: td[0].text.tidy,
       area: td[5].text,
       source: URI.join(url, td[0].css('a/@href').text).to_s,
     }
-    ScraperWiki.save_sqlite([:twitter], data)
   end
 end
 
-scrape_list('http://politwitter.ca/page/canadian-politics-twitters/mp/house')
+def facebook_list(url)
+  noko = noko_for(url)
+  table = noko.xpath('//table[.//th[.="name"]]')
+  table.xpath('.//tr[td]').map do |tr|
+    td = tr.css('td')
+    data = {
+      id: td[1].css('a/@href').text.split('/').last,
+      name: td[1].text.tidy,
+      facebook: td[7].css('a[href*="facebook"]/@href').text
+    }
+  end
+end
+
+def youtube_list(url)
+  noko = noko_for(url)
+  table = noko.xpath('//table[.//th[.="name"]]')
+  table.xpath('.//tr[td]').map do |tr|
+    td = tr.css('td')
+    data = {
+      id: td[1].css('a/@href').text.split('/').last,
+      name: td[1].text.tidy,
+      youtube: td[-1].css('a/@href').text
+    }
+  end
+end
+
+twitter = twitter_list('http://politwitter.ca/page/canadian-politics-twitters/mp/house')
+facebook = facebook_list('http://politwitter.ca/directory/facebook/mp/house')
+youtube = youtube_list('http://politwitter.ca/directory/youtube/mp/house')
+
+data = twitter.group_by { |h| h[:id] }
+
+facebook.each do |h|
+  if data.key? h[:id]
+    data[h[:id]].first.merge! h
+  else 
+    data[h[:id]] = [h]
+  end
+end
+
+youtube.each do |h|
+  if data.key? h[:id]
+    data[h[:id]].first.merge! h
+  else 
+    data[h[:id]] = [h]
+  end
+end
+
+ScraperWiki.save_sqlite([:id], data.values.flatten)
